ef2641649aa800b484eac261bd0ee860e6afb5e3
Maxim Susloparov <susloparov@wireless.iitp.ru>
2020-12-21 23:48:05 +0300

    removed legacy files

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,9 @@
 #include <QtWidgets/QApplication>
-//#include "purse.h"
 #include "ui/mainwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    //Purse w;
-    //w.show();
 
     MainWindow mainWindow;
     mainWindow.show();
